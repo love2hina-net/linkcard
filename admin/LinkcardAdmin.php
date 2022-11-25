691b3c74e5cd0b8307adb413e7e026a1247b828a
love2hina <webmaster@love2hina.net>
2022-11-25 19:46:21 +0900

    公式サンプルを貼り付け

--- admin/LinkcardAdmin.php
+++ admin/LinkcardAdmin.php
@@ -27,7 +27,70 @@ class LinkcardAdmin
 
         $this->plugin->get_loader()->add_action('admin_enqueue_scripts', $this, 'enqueue_styles');
         $this->plugin->get_loader()->add_action('admin_enqueue_scripts', $this, 'enqueue_scripts');
+        $this->plugin->get_loader()->add_action('admin_init', $this, 'admin_init');
         $this->plugin->get_loader()->add_action('admin_menu', $this, 'admin_menu');
+    }
+
+    public function admin_init(): void
+    {
+        \register_setting( 'wporg', 'wporg_options' );
+        \add_settings_section(
+            'wporg_section_developers',
+            __( 'The Matrix has you.', 'wporg' ),
+            [$this, 'wporg_section_developers_callback'],
+            'wporg'
+        );
+        \add_settings_field(
+            'wporg_field_pill',
+            __( 'Pill', 'wporg' ),
+            [$this, 'wporg_field_pill_cb'],
+            'wporg',
+            'wporg_section_developers',
+            array(
+                'label_for'         => 'wporg_field_pill',
+                'class'             => 'wporg_row',
+                'wporg_custom_data' => 'custom',
+            )
+        );
+    }
+
+    public function wporg_section_developers_callback( $args ): void
+    {
+        ?>
+        <p id="<?php echo esc_attr( $args['id'] ); ?>"><?php esc_html_e( 'Follow the white rabbit.', 'wporg' ); ?></p>
+        <?php
+    }
+
+    public function wporg_field_pill_cb( $args )
+    {
+        // Get the value of the setting we've registered with register_setting()
+        $options = get_option( 'wporg_options' );
+        ?>
+        <select
+                id="<?php echo esc_attr( $args['label_for'] ); ?>"
+                data-custom="<?php echo esc_attr( $args['wporg_custom_data'] ); ?>"
+                name="wporg_options[<?php echo esc_attr( $args['label_for'] ); ?>]">
+            <option value="red" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'red', false ) ) : ( '' ); ?>>
+                <?php esc_html_e( 'red pill', 'wporg' ); ?>
+            </option>
+            <option value="blue" <?php echo isset( $options[ $args['label_for'] ] ) ? ( selected( $options[ $args['label_for'] ], 'blue', false ) ) : ( '' ); ?>>
+                <?php esc_html_e( 'blue pill', 'wporg' ); ?>
+            </option>
+        </select>
+        <p class="description">
+            <?php esc_html_e( 'You take the blue pill and the story ends. You wake in your bed and you believe whatever you want to believe.', 'wporg' ); ?>
+        </p>
+        <p class="description">
+            <?php esc_html_e( 'You take the red pill and you stay in Wonderland and I show you how deep the rabbit-hole goes.', 'wporg' ); ?>
+        </p>
+        <?php
+    }
+
+    public function setting_field_callback(array $args): void
+    {
+        echo '<input id="cache_lifetime" name="cache_lifetime[cache_lifetime]" type="number" value="';
+        \form_option('cache_lifetime');
+        echo '" />' . "\n";
     }
 
     public function admin_menu(string $context): void
@@ -44,7 +107,38 @@ class LinkcardAdmin
 
     public function settings_callback(): void
     {
-        echo 'TESTING!';
+        // check user capabilities
+        if ( ! current_user_can( 'manage_options' ) ) {
+            return;
+        }
+
+        // add error/update messages
+
+        // check if the user have submitted the settings
+        // WordPress will add the "settings-updated" $_GET parameter to the url
+        if ( isset( $_GET['settings-updated'] ) ) {
+            // add settings saved message with the class of "updated"
+            add_settings_error( 'wporg_messages', 'wporg_message', __( 'Settings Saved', 'wporg' ), 'updated' );
+        }
+
+        // show error/update messages
+        settings_errors( 'wporg_messages' );
+        ?>
+        <div class="wrap">
+            <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+            <form action="options.php" method="post">
+                <?php
+                // output security fields for the registered setting "wporg"
+                settings_fields( 'wporg' );
+                // output setting sections and their fields
+                // (sections are registered for "wporg", each field is registered to a specific section)
+                do_settings_sections( 'wporg' );
+                // output save settings button
+                submit_button( 'Save Settings' );
+                ?>
+            </form>
+        </div>
+        <?php
     }
 
     /**
